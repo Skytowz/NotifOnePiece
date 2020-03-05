4bc7876f439d42c2b9763d19b9600fc2bee5d59c
Skytowz <46870222+Skytowz@users.noreply.github.com>
2020-03-06 00:38:22 +0100

    v6.0
    gras title info

--- Commandes/info.js
+++ Commandes/info.js
@@ -7,7 +7,7 @@ module.exports.run = async(client, message, args) =>{
     }
     const msg = new Discord.RichEmbed()
         .setColor('#0000ff')
-        .setTitle('Info')
+        .setTitle('**Info**')
         .setDescription(text)
         .setFooter("Le staff : "+ message.author.username)
         .setThumbnail("https://cdn.ter.sncf.com/medias/Images/centre_val_de_loire/Picto%20info%20bleu_tcm56-120300_tcm56-120299_272x194.png");
