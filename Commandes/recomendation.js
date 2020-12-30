62b7160483958a04b61b411870dffcc2b49c1c78
lucas.hottin.etu <lucas.hottin.etu@univ-lille.fr>
2020-12-30 19:52:21 +0100

    fixed bug and do command everywhere

--- Commandes/recomendation.js
+++ Commandes/recomendation.js
@@ -1,12 +1,9 @@
 const Discord = require("discord.js");
 const $ = require('cheerio');
 const rp = require('request-promise');
-const chan = "recommandation";
 
 module.exports.run = async(client, message, args) =>{
-    if(message.channel.name!= chan) return;
     var name;
-    var description;
     var image;
     var text = "";
     var url =args[0];
@@ -14,7 +11,12 @@ module.exports.run = async(client, message, args) =>{
     .then(function(html){
         //success!
         name= $('h1[class=h1titre] span[itemprop=name]',html).html();
-        text="Description:\n"+$('.description',html).text().split(" 1) ")[0].trim();
+        var des = $('.description',html).text().split(" 1) ")[0];
+        text="**Description:**\n";
+        if(des.length>1500){
+            des=des.substr(0,1500).trim()+"..........(see more on Nautiljon)";
+        }
+        text+=des+"\n===================================";
         image ="https://www.nautiljon.com/"+$(`img[alt=\"${name}\"][itemprop=image]`,html)[0].attribs.src;
         var info=$('.mb10',html).html();
         if(info.includes("Licencié en France : ")){
@@ -25,26 +27,26 @@ module.exports.run = async(client, message, args) =>{
             } while (data[i].children[0].data  != "Licencié en France : ");
             if(data[i].parent.children[1].data ==" Oui"){
                 i++;
-                text+="\n\nDisponible légalement sur: ";
+                text+="\n**Disponible légalement sur:** ";
                 var where = data[i];
                 while(where.next != null){
                     where = where.next.next;
                     text+=where.children[0].data;
                     if(where.next!=null) text+=", ";
                 }  
-            }else text+="\n\nPas disponible légalement en France";
-        }else text+="\n\nPas disponible légalement en France";
+            }else text+="\n**Pas disponible légalement en France**";
+        }else text+="\n**Pas disponible légalement en France**";
 
         var nbEp = $("span[itemprop=numberOfEpisodes]",html).text();
-        if(nbEp != "?") text+="\n\nIl y a actuellement "+nbEp+" épisodes";
+        if(nbEp != "?" && nbEp!="") text+="\n\n**Il y a actuellement** "+nbEp+" **épisodes**";
 
         var note = $("span[itemprop=ratingValue]",html).text();
-        if(note!="")text+="\n\nIl possède une note de "+note+"/10";
+        if(note!="")text+="\n\n**Il possède une note de** "+note+"**/10**";
         
         var prodBy = $("span[itemprop=legalName]",info).text();
-        if(prodBy!="") text+="\n\nStudio: "+prodBy;
+        if(prodBy!="") text+="\n\n**Studio: **"+prodBy;
 
-        text+="\n\nNautiljon: "+url;
+        text+="\n\n**Nautiljon: **"+url;
 
         const sugg = new Discord.MessageEmbed()
         .setColor('#fffff')
@@ -52,7 +54,8 @@ module.exports.run = async(client, message, args) =>{
         .setThumbnail(image)
         .setDescription(text)
         .setFooter("Recommended by: "+  message.author.username )
-        .setTimestamp();
+        .setTimestamp()
+        .setURL(url);
 
         message.channel.send(sugg);
 
