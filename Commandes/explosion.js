db54162fc630f58b33aadd2c0dcd2ae47f0491dc
lucas.hottin.etu <lucas.hottin.etu@univ-lille.fr>
2021-01-07 00:03:50 +0100

    test

--- Commandes/explosion.js
+++ Commandes/explosion.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 module.exports.run = async(client, message, args) =>{
     var explosion = false;
-    if(message.member.roles.cache.some(r=>["Test Bot"].includes(r.name)) && explosion) message.edit("explosion");
+    if(message.member.roles.cache.some(r=>["Bot Testeur"].includes(r.name)) && explosion) message.edit("explosion");
     
     //await  message.channel.send({files : ['https://cdn.discordapp.com/attachments/683663363653238794/685450393722814490/b62beab7ce5c03f4503e54bc4923c3fc6d58b0ad_hq.gif']})
 
